a2aa1c6f2f84262c36d7488a831dfe6f38a684a7
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-04-14 19:03:25 +0300

    WAB-8 complete backend setup

---
+++ models/workout_exercise.py
@@ -0,0 +1,9 @@
+from app import db
+
+class WorkoutExercise(db.Model):
+    __tablename__ = "workout_exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    workout_id = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercises.id"), nullable=False)
